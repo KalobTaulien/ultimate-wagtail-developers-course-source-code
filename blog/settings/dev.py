d328465dc70da40f043e90a2090661a6c83ffc1f
Kalob Taulien <kalob.taulien@gmail.com>
2023-09-21 14:41:10 -0600

    Initial setup

---
+++ blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1f(#*=9wybc0053h!vlw4i)t7-jz%c-(h)=v*(elm0w55u7eie"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
