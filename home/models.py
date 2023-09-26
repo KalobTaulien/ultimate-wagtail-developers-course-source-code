36652924e972ece4272db6291df7318ace9d1729
Kalob Taulien <kalob.taulien@gmail.com>
2023-09-26 14:24:34 -0600

    05. Introducing Field Panels

--- home/models.py
+++ home/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
     subtitle = models.CharField(max_length=100, blank=True, null=True)
+    body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('subtitle'),
+        FieldPanel('subtitle', read_only=True),
+        FieldPanel('body'),
     ]
