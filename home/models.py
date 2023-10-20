e8c710e1ed9361df380b76b27ac7216c0e7bc782
Kalob Taulien <kalob.taulien@gmail.com>
2023-10-20 13:59:16 -0600

    15. Linking to Pages

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
@@ -32,9 +33,38 @@ class HomePage(Page):
         related_name='+',
     )
 
+    cta_url = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    cta_external_url = models.URLField(blank=True, null=True)
+
     content_panels = Page.content_panels + [
         FieldPanel('subtitle', read_only=True),
+        FieldPanel('cta_url'),
+        FieldPanel('cta_external_url'),
         FieldPanel('body'),
         FieldPanel('image'),
         FieldPanel('custom_document'),
     ]
+
+    @property
+    def get_cta_url(self):
+        if self.cta_url:
+            return self.cta_url.url
+        elif self.cta_external_url:
+            return self.cta_external_url
+        else:
+            return None
+
+    def clean(self):
+        super().clean()
+
+        if self.cta_url and self.cta_external_url:
+            raise ValidationError({
+                'cta_url': 'You can only have one CTA URL',
+                'cta_external_url': 'You can only have one CTA URL',
+            })
