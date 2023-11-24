95bbba9fecf58218844f4a57a84ebc4a3d30078f
Kalob Taulien <kalob.taulien@gmail.com>
2023-11-24 14:39:09 -0700

    25. Custom StreamBlock Validation

--- blocks/blocks.py
+++ blocks/blocks.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ValidationError
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.blocks import StructBlockValidationError, ListBlockValidationError
 
 
 class TextBlock(blocks.TextBlock):
@@ -8,10 +10,16 @@ class TextBlock(blocks.TextBlock):
         super().__init__(
             **kwargs,
             help_text="This is from my TextBlock (help text is here)",
-            max_length=5,
+            max_length=10,
             min_length=2,
             required=False,
         )
+
+    def clean(self, value):
+        value = super().clean(value)
+        if "wordpress" in value.lower():
+            raise ValidationError("We don't like WordPress here!")
+        return value
 
     class Meta:
         template = "blocks/text_block.html"
@@ -28,16 +36,40 @@ class InfoBlock(blocks.StaticBlock):
         group = "Standalone blocks"
 
 
+
 class FAQBlock(blocks.StructBlock):
     question = blocks.CharBlock()
     answer = blocks.RichTextBlock(
         features=['bold', 'italic']
     )
 
+    def clean(self, value):
+        cleaned_data = super().clean(value)
+        if 'wordpress' in str(cleaned_data['answer']).lower():
+            raise StructBlockValidationError(
+                block_errors={
+                    'answer': ValidationError("We don't like WordPress here!")
+                }
+            )
+        return cleaned_data
+
 
 class FAQListBlock(blocks.ListBlock):
     def __init__(self, **kwargs):
         super().__init__(FAQBlock(), **kwargs)
+
+    def clean(self, value):
+        cleaned_data = super().clean(value)
+        errors = {}
+
+        for index, obj in enumerate(cleaned_data):
+            if 'wordpress' in str(obj['answer']).lower():
+                errors[index] = ValidationError("We don't like WordPress here!")
+
+        if errors:
+            raise ListBlockValidationError(block_errors=errors)
+
+        return cleaned_data
 
     class Meta:
         min_num = 1
@@ -55,6 +87,19 @@ class CarouselBlock(blocks.StreamBlock):
             ('author', blocks.TextBlock()),
         ],
     )
+
+    def clean(self, value):
+        value = super().clean(value)
+        images = [item for item in value if item.block_type == 'image']
+        quotations = [item for item in value if item.block_type == 'quotation']
+
+        if not images or not quotations:
+            raise ValidationError("You must have at least one image and one quotation")
+
+        if len(images) != len(quotations):
+            raise ValidationError("You must have the same number of images and quotations")
+
+        return value
 
     class Meta:
         template = "blocks/carousel_block.html"
