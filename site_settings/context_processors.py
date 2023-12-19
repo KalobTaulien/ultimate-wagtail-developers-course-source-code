9e7c8feeed302c9e08657ec068120415741559c5
Kalob Taulien <kalob.taulien@gmail.com>
2023-12-19 14:03:00 -0700

    44. Fix for showing the right pages based on a Locale

--- site_settings/context_processors.py
+++ site_settings/context_processors.py
@@ -1,6 +1,7 @@
-from wagtail.models import Page
+from wagtail.models import Page, Locale
 
 def navbar(request):
+    locale = Locale.objects.get(language_code=request.LANGUAGE_CODE)
     return {
-        "navbar_pages": Page.objects.live().in_menu().public()
+        "navbar_pages": Page.objects.live().in_menu().public().filter(locale=locale)
     }
