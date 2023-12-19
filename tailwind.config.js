cf3aabd64019cbf62901b40b00ff84b60a8894a4
Kalob Taulien <kalob.taulien@gmail.com>
2023-12-19 10:43:41 -0700

    43. Adding Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{css,js}", "./**/templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
